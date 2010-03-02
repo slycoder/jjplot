6ecede7e889c7a4e7e96d2c4865c42f0107548f6
ebakshy <ebakshy@b99420b2-1bfb-11df-85e6-9b4bd7dd5138>
2010-03-02 00:16:11 +0000

    -added initial support for log-transforming data -- -questionable behavior:  *log transform of x-axis is applied before any stats are applied  *log transform of y-axis data for stats that generate y must log-transform within the function (jjplot.table and .ccdf have been modified accordingly) -todo:  *tick labels are given in terms of 10^increment. still need to figure out how to fancy print these  *it would be nice for ticks to become more dense as x-axis increases, as is in qplot() / plot()
    -added default y-axis labeling for .table() and .ccdf()

--- trunk/jjplot.demo.R
+++ trunk/jjplot.demo.R
@@ -83,7 +83,4 @@ jjplot(x + 2, y, data = df,
 
 # plot ccdf of heavy tailed distribution
 df <- data.frame(x=rlnorm(1000,2,2.5))
-jjplot(x, data = df,
-       ylab=expression(Pr(x >= X)),
-       xlab=expression(log[10](x)),
-       jjplot.ccdf(density=TRUE,log='xy'), jjplot.point())
+jjplot(x, data = df, jjplot.ccdf(density=TRUE), jjplot.point(),log='xy')
