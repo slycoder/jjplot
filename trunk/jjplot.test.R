32b5ff3d66969469f6622ec08e5be72e58e53335
slycoder <slycoder@b99420b2-1bfb-11df-85e6-9b4bd7dd5138>
2010-03-08 05:07:46 +0000

    fix jitter when no y is specified, fix x-labels for factors, divide examples into working and non-working

--- trunk/jjplot.test.R
+++ trunk/jjplot.test.R
@@ -1,9 +1,38 @@
 quartz()
 
+
+### EXAMPLES THAT WORK ###
 data <- data.frame(x = rnorm(100), y = rnorm(100),
                    f = factor(c('A', 'B', 'C', 'D')))
+jjplot( ~ bar(width=0.1) : hist() : jitter(xfactor = 1) + x, data = data)
+
+df <- data.frame(x = 1:50, y = rnorm(50))
+jjplot(y ~ bar(col = "black") + x, data = df, fill = y)
+
+jjplot(y ~ hline(lty = "dashed", col = "red") : fun.y(mean) +
+           point() + line() + x, data = df)
+
+require("reshape")
+df <- data.frame(state = rownames(state.x77),
+                 region = state.region,
+                 t((t(state.x77) - colMeans(state.x77)) /
+                   apply(state.x77, 2, sd)))
+melted <- melt(df, id.vars = c("state", "region"))
+
+jjplot(state ~ point(pch = 22, size = 2) + variable, data = melted,
+       alpha = 1.0, fill = value,
+       ylab = "", xlab = "")
+
+
+### EXAMPLES THAT DON'T WORK ###
 
 source("jjplot.R")
+jjplot(Income ~ box() : group(quantile(), by = region) + region,
+       data = df, fill = region)
+
+
+data <- data.frame(x = rnorm(100), y = rnorm(100),
+                   f = factor(c('A', 'B', 'C', 'D')))
 jjplot(y ~ hline(lty = "dashed") : fun.y(mean) +
            abline() : group(fit(), by = f) +
            point(alpha = 0.2) : jitter(xfactor = 1) +
@@ -11,12 +40,6 @@ jjplot(y ~ hline(lty = "dashed") : fun.y(mean) +
 
 df <- data.frame(x = sample(factor(LETTERS[1:10]), 100, replace=TRUE))
 jjplot( ~ bar(width = 0.5) : table() + x, data = df)
-
-df <- data.frame(x = 1:50, y = rnorm(50))
-jjplot(y ~ bar(col = "black") + x, data = df, fill = y)
-
-jjplot(y ~ hline(lty = "dashed", col = "red") : fun.y(mean) +
-           point() + line() + x, data = df)
 
 df <- data.frame(state = rownames(state.x77),
                  region = state.region,
@@ -31,23 +54,10 @@ jjplot(Income, Murder, data = df,
        jjplot.abline())
 dev.off()
 
-jjplot(Income ~ box() : quantile() | region + region,
-       data = df, fill = region)
-
-require("reshape")
-df <- data.frame(state = rownames(state.x77),
-                 region = state.region,
-                 t((t(state.x77) - colMeans(state.x77)) /
-                   apply(state.x77, 2, sd)))
-melted <- melt(df, id.vars = c("state", "region"))
-
-jjplot(state ~ point(pch = 22, size = 2) + variable, data = melted,
-       alpha = 1.0, fill = value,
-       ylab = "", xlab = "")
-
 df <- data.frame(x = rnorm(10000) + (1:4) * 1,
                  f = factor(c('A', 'B', 'C', 'D')))
 df$y <- 1:4 * df$x + rnorm(10000)
 
 jjplot(f ~ point() : jitter(yfactor=1, xfactor=1) + x,
        data = data, alpha = 1.0, color = f)
+
